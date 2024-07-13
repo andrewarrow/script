46d7c8d224545199a12d42a321e9b6b3b043c1a8
Andrew Arrow <oneone@gmail.com>
2024-07-13 16:55:58 -0700

    improved file validation

--- browser/script.go
+++ browser/script.go
@@ -3,7 +3,7 @@ package browser
 func RunScriptForFly() {
 	guid := Global.Start
 	m := map[string]any{}
-	Document.RenderToId("a"+guid, "widget_us", m)
+	Document.RenderToId("a"+guid, "widget_us_dark", m)
 	// <script src="https://cdn.tailwindcss.com" class="">
 	// document.createElement('script'); script.src =
 	//document.head.appendChild(script)
